5da591e5f0d1db3b5041456d3f1562f980223881
Onur EKER <onur.eker@gmail.com>
2016-04-18 18:40:28 +0300

    Nuget specs added

--- Source/Projects/Bosphorus.Metadata.Class/Properties/AssemblyInfo.cs
+++ Source/Projects/Bosphorus.Metadata.Class/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bosphorus.Metadata.Class")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bosphorus Metadata Class")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bosphorus")]
 [assembly: AssemblyProduct("Bosphorus.Metadata.Class")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Bosphorus")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
--- Source/Projects/Bosphorus.Metadata.Core/Properties/AssemblyInfo.cs
+++ Source/Projects/Bosphorus.Metadata.Core/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bosphorus.Metadata.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bosphorus.Metadata.Core")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Boshporus")]
 [assembly: AssemblyProduct("Bosphorus.Metadata.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Boshporus")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
